41e63d86dedf75af69eee3a14ad3fd02309e85c9
Arun Kuruvilla <arunjohnkuruvilla@gmail.com>
2016-09-21 12:03:11 -0400

    Commit-7

--- stage2.py
+++ stage2.py
@@ -116,31 +116,35 @@ def make_keywords_2(HASH_FILENAME, CHARACTERS_FILENAME, OUTPUT_FILENAME, PASSWOR
 	for x in xrange(1,6):
 		if x == 1:																# Strings of length 1
 			for password in passwords:
-				for char1 in chars:
-					string = str(format(x, '02d')) + char1 + password
-					check_result = check(string, data)
-					if(check_result[0]):
-						print_out(OUTPUT_FILENAME, check_result[1], check_result[2])
-						results[check_result[1]] = check_result[2]
-		if x == 2:																# Strings of length 2
-			for password in passwords:
-				for char1 in chars:
-					for char2 in chars:
-						string = str(format(x, '02d')) + char1 + char2 + password
+				for x in xrange(0,100):
+					for char1 in chars:
+						string = str(format(x, '02d')) + char1 + password
 						check_result = check(string, data)
 						if(check_result[0]):
 							print_out(OUTPUT_FILENAME, check_result[1], check_result[2])
 							results[check_result[1]] = check_result[2]
-		if x == 3:																# Strings of length 3
-			for password in passwords:	
-				for char1 in chars:
-					for char2 in chars:
-						for char3 in chars:
-							string = str(format(x, '02d')) + char1 + char2 + char3 + password 
+
+		if x == 2:																# Strings of length 2
+			for password in passwords:
+				for x in xrange(0,100):
+					for char1 in chars:
+						for char2 in chars:
+							string = str(format(x, '02d')) + char1 + char2 + password
 							check_result = check(string, data)
 							if(check_result[0]):
 								print_out(OUTPUT_FILENAME, check_result[1], check_result[2])
 								results[check_result[1]] = check_result[2]
+		if x == 3:																# Strings of length 3
+			for password in passwords:	
+				for x in xrange(0,100):
+					for char1 in chars:
+						for char2 in chars:
+							for char3 in chars:
+								string = str(format(x, '02d')) + char1 + char2 + char3 + password 
+								check_result = check(string, data)
+								if(check_result[0]):
+									print_out(OUTPUT_FILENAME, check_result[1], check_result[2])
+									results[check_result[1]] = check_result[2]
 	return results
 
 '''def make_keywords_3(HASH_FILENAME, CHARACTERS_FILENAME, OUTPUT_FILENAME, PASSWORD_FILE):
